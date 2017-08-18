300016d3a978fafd42ff328a6557c79bebaa18e1
VincentHokie <VincentHokie@users.noreply.github.com>
2017-08-18 14:09:50 +0300

    Requests given correct syntax

--- Post_and_comments.py
+++ Post_and_comments.py
@@ -4,7 +4,7 @@ import requests
 def get_posts(parameter):
     ENDPOINT = "http://34.207.10.230:3000/posts"
     if parameter is not None and isinstance(parameter, int):
-        ENDPOINT = "http://34.207.10.230:3000/posts/id=" + parameter
+        ENDPOINT = "http://34.207.10.230:3000/posts/?id=" + parameter
         response = requests.get(ENDPOINT)
     return response.text
 
@@ -12,6 +12,6 @@ def get_posts(parameter):
 def get_comments(parameter):
     ENDPOINT = "http://34.207.10.230:3000/comments"
     if parameter is not None and isinstance(parameter, int):
-        ENDPOINT = "http://34.207.10.230:3000/comments/postId=" + parameter
+        ENDPOINT = "http://34.207.10.230:3000/comments/?postId=" + parameter
         response = requests.get(ENDPOINT)
     return response.text
