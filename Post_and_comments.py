5aee7b51f486a06bd2213a92756739bb0cd9fe12
otekat <heskmanuel@gmail.com>
2017-08-18 12:14:54 +0300

    adding README file

--- Post_and_comments.py
+++ Post_and_comments.py
@@ -1,13 +1,17 @@
 import requests
 
 
-def get_posts():
+def get_posts(parameter):
     endpoint = "http://34.207.10.230:3000/posts "
     response = requests.get(endpoint)
+    if parameter is not None and isinstance(parameter, int):
+        endpoint = "http://34.207.10.230:3000/comments/postId=" + parameter
     return response.text
 
 
-def get_comments():
+def get_comments(parameter):
     endpoint = "http://34.207.10.230:3000/comments"
     response = requests.get(endpoint)
+    if parameter is not None and isinstance(parameter, int):
+        endpoint = "http://34.207.10.230:3000/comments/postId=" + parameter
     return response.text
